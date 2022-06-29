6042933b134292cf56da3ce73afd8071bdbcaa14
Arthur Jakypbek <arthurjkk43@gmail.com>
2022-06-29 17:22:45 +0600

    initial commit

---
+++ src/main/java/com/megacom/hotelreservationprojectmainmasterfinal/HotelReservationProjectMainMasterFinalApplication.java
@@ -0,0 +1,13 @@
+package com.megacom.hotelreservationprojectmainmasterfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelReservationProjectMainMasterFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelReservationProjectMainMasterFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/megacom/hotelreservationprojectmainmasterfinal/HotelReservationProjectMainMasterFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.megacom.hotelreservationprojectmainmasterfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelReservationProjectMainMasterFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
